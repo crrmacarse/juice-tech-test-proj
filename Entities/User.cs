746bff90a2ed5c6d98e58bf029fcd1c4c225558d
crrmacarse <crrmacarse@gmail.com>
2025-02-25 03:28:46 +0800

    feat: initial user setup

---
+++ Entities/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication1.Entities;
+
+public class User
+{
+    public string Username { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+
+}
