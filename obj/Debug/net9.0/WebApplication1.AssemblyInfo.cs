d3f04e9419cb4dabc93a8ff9820f596dfaacef2b
crrmacarse <crrmacarse@gmail.com>
2025-02-24 21:17:02 +0800

    feat: replaced swagger to scalar

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e89df624b9a1a567bef0cf9d2e10be7e16cf7e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b70268521d7ee7d7302872c03da2a35af76aa9f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
