55e78adacb9614e6cbbf1680672e847c7a2ac75b
crrmacarse <crrmacarse@gmail.com>
2025-02-25 05:54:05 +0800

    feat: added db connection

---
+++ Migrations/20250224214256_init-2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_user",
+                table: "user");
+
+            migrationBuilder.RenameTable(
+                name: "user",
+                newName: "users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "user");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_user",
+                table: "user",
+                column: "ID");
+        }
+    }
+}
