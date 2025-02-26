663efcfeac55b69175ea1ee2ac20373aa9e6cfe0
crrmacarse <crrmacarse@gmail.com>
2025-02-26 10:58:59 +0800

    feat: add role based

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Entities;
@@ -18,6 +19,12 @@ namespace WebApplication1
         [HttpGet("user")]
         public ActionResult<Product[]> GetProductsByUser() {
             return Ok();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("admin")]
+        public ActionResult GetProductByAdminOnly() {
+            return Ok("Admin only");
         }
     }
 }
